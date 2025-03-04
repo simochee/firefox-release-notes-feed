be94b3a79c70c6ce2857d0e5d32307877b18e12a
simochee <lollipop@simochee.net>
2025-03-05 01:00:16 +0900

    refactor: build with hono

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
-import { mkdir, writeFile } from "node:fs/promises";
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
 import { XMLBuilder } from "fast-xml-parser";
+import { Hono } from "hono";
+import { toSSG } from "hono/ssg";
 import { type BuildNoteOptions, buildChannel } from "./builder";
 
 const FIREFOX_CHANNELS = ["Release", "Beta", "Nightly"] as const;
 const FIREFOX_PRODUCTS: Omit<BuildNoteOptions, "channel">[] = [
 	{
 		product: "Firefox",
-		dir: "./",
+		dir: "/",
 		releaseNoteUrl:
 			"https://www.mozilla.org/en-US/firefox/{version}/releasenotes/",
 	},
 	{
 		product: "Firefox for Android",
-		dir: "./android/",
+		dir: "/android/",
 		releaseNoteUrl:
 			"https://www.mozilla.org/en-US/firefox/android/{version}/releasenotes/",
 	},
 	{
 		product: "Firefox for iOS",
-		dir: "./ios/",
+		dir: "/ios/",
 		releaseNoteUrl:
 			"https://www.mozilla.org/en-US/firefox/ios/{version}/releasenotes/",
 	},
@@ -31,9 +34,17 @@ const builder = new XMLBuilder({
 	format: process.env.NODE_ENV === "development",
 });
 
-await Promise.all(
-	FIREFOX_CHANNELS.map(async (channel) =>
-		FIREFOX_PRODUCTS.map(async (product) => {
+const app = new Hono();
+
+for (const channeName of FIREFOX_CHANNELS) {
+	for (const product of FIREFOX_PRODUCTS) {
+		app.get(`${product.dir}${channeName.toLowerCase()}.xml`, async (c) => {
+			const channel = await buildChannel({ channel: channeName, ...product });
+
+			if (!channel.item || channel.item.length === 0) {
+				return c.notFound();
+			}
+
 			const rss = builder.build({
 				"?xml": {
 					"@_version": "1.0",
@@ -41,15 +52,20 @@ await Promise.all(
 				},
 				rss: {
 					"@_version": "2.0",
-					channel: await buildChannel({ channel, ...product }),
+					channel,
 				},
 			});
 
-			const dist = new URL("../dist/", import.meta.url);
-			const feed = new URL(product.dir, dist);
+			return c.body(rss, 200, {
+				"Content-Type": "application/xml",
+			});
+		});
+	}
+}
 
-			await mkdir(feed, { recursive: true });
-			await writeFile(new URL(`${channel.toLowerCase()}.xml`, feed), rss);
-		}),
-	),
-);
+toSSG(app, fs, {
+	dir: fileURLToPath(new URL("../dist", import.meta.url)),
+	afterResponseHook(res) {
+		return res.status === 200 ? res : false;
+	},
+});
