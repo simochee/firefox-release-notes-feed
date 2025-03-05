196985c59bddb9aac55bd5106b5a6ab37a20958b
simochee <lollipop@simochee.net>
2025-03-05 12:38:51 +0900

    fix: xml attr

--- src/rss.ts
+++ src/rss.ts
@@ -20,6 +20,7 @@ export const buildRSS = async (
 		"?xml": {
 			"@_version": "1.0",
 			"@_encoding": "UTF-8",
+			"@_xmlns:media": "http://search.yahoo.com/mrss/",
 		},
 		rss: {
 			"@_version": "2.0",
